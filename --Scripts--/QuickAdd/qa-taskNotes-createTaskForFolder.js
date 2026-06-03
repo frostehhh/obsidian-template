1dcaa0540c8e5581a1f44fdbe0c199cf2028ab66
frostehhh <frostehhh967@gmail.com>
2026-06-03 14:25:33 +0800

    Update QuickAdd qa-taskNotes-createTaskForFolder
    Add projects prop on create of task

--- --Scripts--/QuickAdd/qa-taskNotes-createTaskForFolder.js
+++ --Scripts--/QuickAdd/qa-taskNotes-createTaskForFolder.js
@@ -8,6 +8,7 @@ module.exports = async (params) => {
   }
 
   const folderPath = file.parent.path === "/" ? "" : file.parent.path;
+  const folderName = file.parent.name;
   const tasksFolder = folderPath ? `${folderPath}/Tasks` : "Tasks";
 
   const taskTitle = await quickAddApi.inputPrompt("Task title");
@@ -19,16 +20,25 @@ module.exports = async (params) => {
     return;
   }
 
+  const projectFilePath = folderPath ? `${folderPath}/${folderName}.md` : `${folderName}.md`;
+  const projectFile = app.vault.getAbstractFileByPath(projectFilePath);
+  const projects = projectFile ? [`[[${folderName}]]`] : [];
+
   if (!app.vault.getAbstractFileByPath(tasksFolder)) {
     await app.vault.createFolder(tasksFolder);
   }
 
-  const task = await api.tasks.create({ title: taskTitle });
+  const task = await api.tasks.create({ title: taskTitle, projects });
   await api.tasks.move(task.path, tasksFolder);
 
   const movedPath = `${tasksFolder}/${task.path.split("/").pop()}`;
   const movedFile = app.vault.getAbstractFileByPath(movedPath);
   if (movedFile) {
+    if (projects.length) {
+      await app.fileManager.processFrontMatter(movedFile, (fm) => {
+        fm.projects = projects;
+      });
+    }
     const leaf = app.workspace.getLeaf(false);
     await leaf.openFile(movedFile);
   }
